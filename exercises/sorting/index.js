051669128bcec56b51526317614d588d0790a764
maribelduran <maribelduranucm@gmail.com>
2020-01-04 10:40:28 -0800

    2019-selection-sort-solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,23 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin != i) {
+      let temp = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+}
 
 function merge(left, right) {}
 
