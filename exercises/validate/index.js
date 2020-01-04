bdad151056f30924e690966354d61d993e094ebf
maribelduran <maribelduranucm@gmail.com>
2020-01-04 09:12:28 -0800

    2019 solution

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -6,16 +6,16 @@
 // the parent
 
 function validate(node, min = null, max = null) {
-  if (max !== null && node.data > max){
+  if (max != null && node.data > max) {
     return false;
   }
-  if (min !== null && node.data < min){
+  if (min != null && node.data < min) {
     return false;
   }
-  if (node.left && !validate(node.left, min, node.data)){
+  if (node.left && !validate(node.left, min, node.data)) {
     return false;
   }
-  if (node.right && !validate(node.right, node.data, max)){
+  if (node.right && !validate(node.right, node.data, max)) {
     return false;
   }
   return true;
