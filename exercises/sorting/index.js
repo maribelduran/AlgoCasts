e42188d3990650ed09496f353379cb89ec7935a1
maribelduran <maribelduranucm@gmail.com>
2020-01-04 10:55:32 -0800

    2019-merge-sort-solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -44,9 +44,18 @@ function merge(left, right) {
     }
   }
 
-  return [...results, ...let, ...right];
+  return [...results, ...left, ...right];
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
