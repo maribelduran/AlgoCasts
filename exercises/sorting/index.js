798e5595f4d33b68394a9bd00c4c2a893b45f452
maribelduran <maribelduranucm@gmail.com>
2020-01-04 10:44:08 -0800

    2019-merge-function-solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -34,7 +34,18 @@ function selectionSort(arr) {
   return arr;
 }
 
-function merge(left, right) {}
+function merge(left, right) {
+  const results = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...let, ...right];
+}
 
 function mergeSort(arr) {}
 
